0c597e78fe651d2692757c8b1bd4fec9ec65ce8b
bedigital@mac.com <brian.uckert@be-digital.biz>
2021-01-16 15:03:33 -0500

    update

--- main.tf
+++ main.tf
@@ -5,8 +5,27 @@ resource "aws_instance" "example" {
   ami           = "ami-0885b1f6bd170450c"
   instance_type = "t2.micro"
 
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags = {
       Name = "terraform-example"
   }
+}
 
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
