d8a08edc5a20b52126d54cb63ec949cdf402a116
bedigital@mac.com <brian.uckert@be-digital.biz>
2021-01-17 09:06:37 -0500

    version two

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 provider "aws" {
   region = "us-east-1"
-}
-resource "aws_instance" "example" {
-  ami           = "ami-0885b1f6bd170450c"
-  instance_type = "t2.micro"
 
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0885b1f6bd170450c"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
-      Name = "terraform-example"
+    Name = "terraform-example"
   }
 }
 
@@ -23,20 +24,10 @@ resource "aws_security_group" "instance" {
   name = var.security_group_name
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-variable "security_group_name" {
-  description = "The name of the security group"
-  type        = string
-  default     = "terraform-example-instance"
-}
-
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the Instance"
-}
